12767c11817b7b643111bf23dc060c827083eb26
Sesugh Asa <74496255+elsesu@users.noreply.github.com>
2025-08-28 15:59:43 +0200

    again

--- src/components/Sponsors/SponsorCard.js
+++ src/components/Sponsors/SponsorCard.js
@@ -2,6 +2,9 @@
 import { css, jsx } from "@emotion/core";
 import React from "react";
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 7112a83 (again)
 import LogoOne from "../PARTNERS/FB_IMG_16383071447628811.jpg";
 import LogoTwo from "../PARTNERS/IMG-20250427-WA0008.jpg";
 import LogoThree from "../PARTNERS/IMG-20250708-WA0009.jpg";
@@ -10,6 +13,7 @@ import LogoFive from "../PARTNERS/Picture2.jpg";
 import LogoSix from "../PARTNERS/aquatrust.jpg";
 import LogoSeven from "../PARTNERS/FB_IMG_16383071447628811.jpg";
 import LogoEight from "../PARTNERS/IMG-20250708-WA0009.jpg";
+<<<<<<< HEAD
 =======
 import LogoOne from "../../img/logoOne.png";
 import LogoTwo from "../../img/logoTwo.png";
@@ -20,6 +24,8 @@ import LogoSix from "../../img/logoSix.png";
 import LogoSeven from "../../img/logoSeven.png";
 import LogoEight from "../../img/logoEight.png";
 >>>>>>> f6e17d3 (ready)
+=======
+>>>>>>> 7112a83 (again)
 
 const SponsorCard = () => {
   const logos = [
@@ -51,10 +57,14 @@ const styles = css`
   border: 1px solid #444675;
   display: flex;
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 7112a83 (again)
   align-items: center;
   justify-content: center;
   overflow: hidden;
 
+<<<<<<< HEAD
   img {
     max-width: 80%;
     max-height: 80%;
@@ -63,6 +73,12 @@ const styles = css`
   img {
     margin: auto;
 >>>>>>> f6e17d3 (ready)
+=======
+  img {
+    max-width: 80%;
+    max-height: 80%;
+    object-fit: contain;
+>>>>>>> 7112a83 (again)
     display: block;
     cursor: pointer;
     opacity: 0.4;
@@ -71,6 +87,7 @@ const styles = css`
       opacity: 1;
     }
   }
+<<<<<<< HEAD
 <<<<<<< HEAD
 
   @media (max-width: 700px) {
@@ -84,20 +101,34 @@ const styles = css`
     &:nth-child(8) {
 =======
   @media (max-width: 700px){
+=======
+
+  @media (max-width: 700px) {
+>>>>>>> 7112a83 (again)
     max-width: 50%;
   }
-  @media (min-width: 701px) and (max-width: 1020px){
+
+  @media (min-width: 701px) and (max-width: 1020px) {
     max-width: 32%;
     margin: 10px 0;
+<<<<<<< HEAD
     &:nth-child(7), &:nth-child(8){
 >>>>>>> f6e17d3 (ready)
+=======
+    &:nth-child(7),
+    &:nth-child(8) {
+>>>>>>> 7112a83 (again)
       max-width: 49%;
     }
   }
 `;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
 >>>>>>> f6e17d3 (ready)
+=======
+
+>>>>>>> 7112a83 (again)
 export default SponsorCard;
